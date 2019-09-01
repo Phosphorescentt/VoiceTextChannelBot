d6cb2abf9f803962ae96abd52f0b19d380bbcbe8
Phosphorescentt <joshua@mankelow.org>
2019-09-01 19:33:34 +0100

    Added functionality to create channels of a given name when a text message has the appropriate command prefix

--- main.py
+++ main.py
@@ -9,17 +9,29 @@ from SECRETS import *
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
+        self.commands = {"create_channel": self.create_channel}
 
     async def on_message(self, message):
         # don't respond to ourselves
         if message.author == self.user:
             return
 
-        if message.content == "wad":
-            message.content = message.content.replace(">>>", "")
+        message.content = message.content.lower()
+
+        if message.content.startswith(">>>"):
             guild = message.guild
-            await guild.create_text_channel(name=message.content)
-            await guild.create_voice_channel(name=message.content)
+            command_raw = message.content.replace(">>>", "")
+            command = command_raw.split()
+    
+            try:
+                execute = self.commands[command[0]]
+                await execute(guild, command)
+            except KeyError as e:
+                print(e)
+
+    async def create_channel(self, guild, command):
+        await guild.create_text_channel(name=command[1])
+        await guild.create_voice_channel(name=command[1])
 
 
 client = MyClient()
